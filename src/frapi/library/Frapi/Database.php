150980cf25cd5263757fe9543620777fa448a42b
David Coallier <david@echolibre.com>
2010-05-14 11:17:31 +0100

    Added a namespace emulation to the database instantiator. Acts as a singleton unless you need another instance in a different namespace

--- src/frapi/library/Frapi/Database.php
+++ src/frapi/library/Frapi/Database.php
@@ -29,7 +29,7 @@ class Frapi_Database extends PDO
      *
      * @var PDO object
      */
-    private static $instance;
+    private static $instance = array();
     
     /**
      * Singleton for database connections
@@ -37,23 +37,24 @@ class Frapi_Database extends PDO
      * This method is going to verify if there's an existing instance
      * of a database object and if so will return it.
      *
+     * @param  string $name The name of the instantiation (namespace emulation)
+     *                      This parameter is optional and is defaulted to 'default'
      *
      * @return PDO A new PDO object or an existing PDO object
      */
-    protected static function singleton()
+    protected static function factory($name = 'default')
     {
-        self::$instance = null;
-        if (!isset(self::$instance)) {
+        if (!isset(self::$instance[$name])) {
             $configs = self::getDbConfig();
 
-            self::$instance = new PDO(
+            self::$instance[$name] = new PDO(
                 'mysql:dbname='.$configs['db_database'].';host='.$configs['db_hostname'], 
                 $configs['db_username'], 
                 $configs['db_password']
             );
         }
         
-        return self::$instance;
+        return self::$instance[$name];
     }
     
     /**
@@ -63,25 +64,19 @@ class Frapi_Database extends PDO
      * of simplicity we are using PDO by default. Thus returning a PDO
      * object (or whichever of it's kind)
      *
+     * @param  string $name The name of the instantiation (namespace emulation)
+     *                      This parameter is optional and is defaulted to 'default'
      *
      * @return mixed PDO* An object of type PDO
      */
-    public static function getInstance()
+    public static function getInstance($name = 'default')
     {
-        return self::singleton();
+        return self::factory($name);
     }
 
     
     private static function getDbConfig()
     {
-        throw new Frapi_Database_Exception('This method is outdated. We have to fix it.');
-        
-        $sql    = "
-            SELECT key, value 
-            FROM configurations
-            WHERE key IN ('db_hostname', 'db_username', 'db_database', 'db_password')
-        ";
-        
         $configs = Frapi_Internal::getDB()->query($sql)->fetchAll();
         
         $conf = array();
