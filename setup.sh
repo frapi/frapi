0d0a41bd29a2cc23e1f04525907d14c9031d6a3d
System Administrator <root@daithi-ogallchobairs-macbook.local>
2010-05-14 14:11:50 +0100

    Made the chmod 766 instead of 744... apache did not have access to write

---
+++ setup.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+##
+# This is obviously not a very advanced installer
+# in fact it doesn't install much. 
+#
+# If you would be interested in making an actual installer
+# please send an email to our developers mailing list at
+# http://groups.google.com/group/frapi-dev
+##
+
+CURRENTDIR=`pwd`
+
+# Giving permissions
+## This is for the generated actions (By the administration interface)
+chmod -R 766 $CURRENTDIR/src/frapi/custom/Action
+
+## This is for the likes of errors and actions 
+## saved by the administrator interface
+chmod -R 766 $CURRENTDIR/src/frapi/admin/application/config/app
