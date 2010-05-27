1d69d0d275ce2ac4dc4cb61c5f83ab2748c669b7
David Coallier <david@echolibre.com>
2010-05-27 15:32:29 +0100

    - Regarding previous commit, I added the error.phtml file in case one doesn't have write access to the actions.xml - Added the Testing2 example

---
+++ src/frapi/custom/Action/Testing2.php
@@ -0,0 +1,147 @@
+<?php
+
+/**
+ * Action Testing2 
+ * 
+ * Testing 2
+ * 
+ * @link http://getfrapi.com
+ * @author Frapi <frapi@getfrapi.com>
+ * @link /testing/2
+ */
+class Action_Testing2 extends Frapi_Action implements Frapi_Action_Interface
+{
+
+    /**
+     * Required parameters
+     * 
+     * @var An array of required parameters.
+     */
+    protected $requiredParams = array('name');
+
+    /**
+     * The data container to use in toArray()
+     * 
+     * @var A container of data to fill and return in toArray()
+     */
+    private $data = array();
+
+    /**
+     * To Array
+     * 
+     * This method returns the value found in the database 
+     * into an associative array.
+     * 
+     * @return array
+     */
+    public function toArray()
+    {
+        $this->data['name'] = $this->getParam('name', self::TYPE_OUTPUT);
+        return $this->data;
+    }
+
+    /**
+     * Default Call Method
+     * 
+     * This method is called when no specific request handler has been found
+     * 
+     * @return array
+     */
+    public function executeAction()
+    {
+        $valid = $this->hasRequiredParameters($this->requiredParams);
+        if ($valid instanceof Frapi_Error) {
+            return $valid;
+        }
+        
+        return $this->toArray();
+    }
+
+    /**
+     * Get Request Handler
+     * 
+     * This method is called when a request is a GET
+     * 
+     * @return array
+     */
+    public function executeGet()
+    {
+        $valid = $this->hasRequiredParameters($this->requiredParams);
+        if ($valid instanceof Frapi_Error) {
+            return $valid;
+        }
+        
+        return $this->toArray();
+    }
+
+    /**
+     * Post Request Handler
+     * 
+     * This method is called when a request is a POST
+     * 
+     * @return array
+     */
+    public function executePost()
+    {
+        $valid = $this->hasRequiredParameters($this->requiredParams);
+        if ($valid instanceof Frapi_Error) {
+            return $valid;
+        }
+        
+        return $this->toArray();
+    }
+
+    /**
+     * Put Request Handler
+     * 
+     * This method is called when a request is a PUT
+     * 
+     * @return array
+     */
+    public function executePut()
+    {
+        $valid = $this->hasRequiredParameters($this->requiredParams);
+        if ($valid instanceof Frapi_Error) {
+            return $valid;
+        }
+        
+        return $this->toArray();
+    }
+
+    /**
+     * Delete Request Handler
+     * 
+     * This method is called when a request is a DELETE
+     * 
+     * @return array
+     */
+    public function executeDelete()
+    {
+        $valid = $this->hasRequiredParameters($this->requiredParams);
+        if ($valid instanceof Frapi_Error) {
+            return $valid;
+        }
+        
+        return $this->toArray();
+    }
+
+    /**
+     * Head Request Handler
+     * 
+     * This method is called when a request is a HEAD
+     * 
+     * @return array
+     */
+    public function executeHead()
+    {
+        $valid = $this->hasRequiredParameters($this->requiredParams);
+        if ($valid instanceof Frapi_Error) {
+            return $valid;
+        }
+        
+        return $this->toArray();
+    }
+
+
+}
+
