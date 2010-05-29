972d3ba3afc36eb184f10caedcfe463227c4317c
Jeremy Kendall <jeremy@jeremykendall.net>
2010-05-30 05:02:00 +0800

    Minor tweaks, modified documentation,  including setting error_reporting to -1 for testing and development

--- src/frapi/library/Frapi/Exception.php
+++ src/frapi/library/Frapi/Exception.php
@@ -21,21 +21,21 @@ class Frapi_Exception extends Exception
     /**
      * Where the error happened
      *
-     * @var string $at  Where did that error actually happen?
+     * @var string
      */
     protected $at;
     
     /**
      * HTTP Response Code
      *
-     * @var int Default to 400 if nothing was mentionned ;-)
+     * @var int
      */
-    protected $http_code = 400;
+    protected $http_code;
     
     /**
      * The name of the actual exception
      *
-     * @var string The name of the exception
+     * @var string
      */
     protected $name;
     
@@ -46,10 +46,10 @@ class Frapi_Exception extends Exception
      * messages to Frapi_Error.  Frapi_Exception is only used internally. API
      * developers should use Frapi_Error for providing errors to consumers.
      * 
-     * @param string $message
-     * @param string $name
-     * @param int $http_code
-     * @param string $at
+     * @param string $message   Exception message
+     * @param string $name      The name of the exception
+     * @param int    $http_code Defaults is 400
+     * @param string $at        Where did that error actually happen?
      */
     public function __construct($message, $name, $http_code = 400, $at = '')
     {
