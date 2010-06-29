44350771d3ac171dbf2f64290733f8f065f017fa
David Coallier <david@echolibre.com>
2010-06-29 22:53:25 +0100

    - Fixed windows problematic paths by using DIRECTORY_SEPARATOR

--- src/frapi/admin/public/index.php
+++ src/frapi/admin/public/index.php
@@ -1,7 +1,8 @@
 <?php
 // Define path to application directory
 define('ROOT_PATH',        dirname(dirname(dirname(__FILE__))));
-define('APPLICATION_PATH', ROOT_PATH . '/admin/application');
+define('APPLICATION_PATH', ROOT_PATH . DIRECTORY_SEPARATOR . 
+                           'admin' . DIRECTORY_SEPARATOR.'application');
 
 // Define application environment
 define('APPLICATION_ENV', (getenv('APPLICATION_ENV') ? getenv('APPLICATION_ENV') : 'production'));
@@ -14,5 +15,9 @@ require_once 'Zend/Application.php';
 require_once ROOT_PATH . DIRECTORY_SEPARATOR . 'library' . 
              DIRECTORY_SEPARATOR . 'Frapi' . DIRECTORY_SEPARATOR . 'AllFiles.php';
 
-$app = new Zend_Application(APPLICATION_ENV, APPLICATION_PATH . '/config/application.ini');
+$app = new Zend_Application(
+    APPLICATION_ENV, APPLICATION_PATH . DIRECTORY_SEPARATOR . 'config' . 
+    DIRECTORY_SEPARATOR.'application.ini'
+);
+
 $app->bootstrap()->run();
