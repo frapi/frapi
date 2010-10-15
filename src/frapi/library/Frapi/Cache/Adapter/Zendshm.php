1fb43b0196dd51cf8ef33c8d5295906002e54de5
David Coallier <david@echolibre.com>
2010-10-15 13:48:02 +0100

    - Added Zend Server support for Zend SHM (Memory caching) or Zend Disk Caching.

---
+++ src/frapi/library/Frapi/Cache/Adapter/Zendshm.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * Frapi Zend Server SHM Cache Adapter Layer
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://getfrapi.com/license/new-bsd
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@getfrapi.com so we can send you a copy immediately.
+ *
+ * The Zend Server SHM adapter of the Frapi Cache.
+ *
+ * @license   New BSD
+ * @copyright echolibre ltd.
+ * @link      http://files.zend.com/help/Zend-Platform/zend_cache_functions.htm
+ * @package   frapi
+ */
+class Frapi_Cache_Adapter_Zendshm implements Frapi_Cache_Interface 
+{
+    /**
+     * Get a cache variable
+     *
+     * Retrieve a variable from the cache and return it's
+     * value to the user.
+     *
+     * @param  string $name  The name of the cache variable to retrieve.
+     *
+     * @return mixed         The value of the retrieved variable or false if
+     *                       variable isn't found.
+     */
+    public function get($name) 
+    {
+        return zend_shm_cache_fetch($name);
+    }
+
+    /**
+     * Add to the cache
+     *
+     * Add a new variable to the cache that you will then be able
+     * to retrieve using the $this->get($name) method.
+     *
+     * @param  string  $name   The name of the cache variable to store.
+     * @param  string  $value  The value of the cache variable to store.
+     * @param  integer $expire When should it expire? Default: 900 seconds.
+     * 
+     * @return boolean       Depending on the success of the operation, 
+     *                       either true or false. 
+     */
+    public function add($name, $value, $expiry = 900) 
+    {
+        return zend_shm_cache_store($name, $value, $expiry);
+    }
+    
+    /**
+     * Delete from the cache
+     *
+     * Delete a variable from the cache so it is no longer usuable and
+     * retrievable by $this->get($name)
+     *
+     * @param  string $name  The name of the cache variable to delete.
+     * 
+     * @return boolean       Depending on the success of the operation, 
+     *                       either true or false. 
+     */
+    public function delete($name) 
+    {
+        return zend_disk_cache_delete($name);
+    }
+}
