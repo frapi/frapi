4da9e1b34a8c52841752256fcb075c289ddf601a
Trevor Morse <trevor.morse@gmail.com>
2010-10-22 13:50:41 -0300

    Updating Partner Controller for console to use views

--- src/frapi/admin/application/modules/console/controllers/PartnerController.php
+++ src/frapi/admin/application/modules/console/controllers/PartnerController.php
@@ -26,50 +26,11 @@ class PartnerController extends Zend_Controller_Action
      */
     public function listAction()
     {
-        $this->_helper->viewRenderer->setNoRender();
+        $this->_helper->viewRenderer->setViewSuffix('txt');
 
-        $model      = new Default_Model_Partner();
-        $partners = $model->getAll();
-
-        // Determine our max field widths so we can pad things out appropriately
-        $partner_first_name_max_length = strlen('First Name ');
-        $partner_last_name_max_length  = strlen('Last Name ');
-        $partner_email_max_length      = strlen('Email ');
-        $partner_company_max_length    = strlen('Company ');
-
-        if ($partners) {
-            foreach ($partners as $key => $partner) {
-
-                if (strlen($partner['firstname']) > $partner_first_name_max_length) {
-                    $partner_first_name_max_length = strlen($partner['firstname']) + 1;
-                }
-
-                if (strlen($partner['lastname']) > $partner_last_name_max_length) {
-                    $partner_last_name_max_length = strlen($partner['lastname']) + 1;
-                }
-
-                if (strlen($partner['email']) > $partner_email_max_length) {
-                    $partner_email_max_length = strlen($partner['email']) + 1;
-                }
-
-                if (strlen($partner['company']) > $partner_company_max_length) {
-                    $partner_company_max_length = strlen($partner['company']) + 1;
-                }
-            }
-        }
-        echo  str_pad('First Name', $partner_first_name_max_length) .
-            str_pad('Last Name', $partner_last_name_max_length) .
-            str_pad('Email', $partner_email_max_length) .
-            str_pad('Company', $partner_company_max_length) . PHP_EOL;
-
-        if ($partners) {
-            foreach ($partners as $key => $partner) {
-                echo str_pad($partner['firstname'], $partner_first_name_max_length) .
-                    str_pad($partner['lastname'], $partner_last_name_max_length) .
-                    str_pad($partner['email'], $partner_email_max_length) .
-                    str_pad($partner['company'], $partner_company_max_length) . PHP_EOL;
-            }
-        }
+        $model           = new Default_Model_Partner();
+        $this->view
+              ->partners = $model->getAll();
     }
 
     /**
@@ -82,7 +43,7 @@ class PartnerController extends Zend_Controller_Action
      */
     public function addAction()
     {
-        $this->_helper->viewRenderer->setNoRender();
+        $this->_helper->viewRenderer->setViewSuffix('txt');
 
         // The options we are accepting for adding
         $options = new Zend_Console_Getopt(
@@ -97,16 +58,17 @@ class PartnerController extends Zend_Controller_Action
         try {
             $options->parse();
         } catch (Zend_Console_Getopt_Exception $e) {
-            echo $e->getUsageMessage();
-            exit();
+            $this->view->message = $e->getUsageMessage();
+            $this->render();
+            return;
         }
 
         if ($options->getOption('first-name') == ''
             || $options->getOption('last-name') == ''
             || $options->email == ''
             || $options->company == '') {
-            echo $options->getUsageMessage();
-            exit();
+            $this->view->message = $options->getUsageMessage();
+            return;
         }
 
         $partner_first_name = $options->getOption('first-name');
@@ -124,9 +86,9 @@ class PartnerController extends Zend_Controller_Action
         $model = new Default_Model_Partner();
         try {
             $model->add($submit_data);
-            echo 'Successfully added partner: ' . $partner_email . PHP_EOL;
+            $this->view->message = 'Successfully added partner: ' . $partner_email . PHP_EOL;
         } catch (RuntimeException $e) {
-            echo 'Error adding partner: ' . $partner_email . '. ' . $e->getMessage() . PHP_EOL;
+            $this->view->message = 'Error adding partner: ' . $partner_email . '. ' . $e->getMessage() . PHP_EOL;
         }
     }
 
@@ -139,7 +101,7 @@ class PartnerController extends Zend_Controller_Action
      */
     public function deleteAction()
     {
-        $this->_helper->viewRenderer->setNoRender();
+        $this->_helper->viewRenderer->setViewSuffix('txt');
 
         // The options we are accepting for deleting
         $options = new Zend_Console_Getopt(
@@ -151,8 +113,8 @@ class PartnerController extends Zend_Controller_Action
         try {
             $options->parse();
         } catch (Zend_Console_Getopt_Exception $e) {
-            echo $e->getUsageMessage();
-            exit();
+            $this->view->message = $e->getUsageMessage();
+            return;
         }
         if ($options->email == '') {
             echo $options->getUsageMessage();
@@ -172,15 +134,15 @@ class PartnerController extends Zend_Controller_Action
         }
 
         if (!$partner_id) {
-            echo 'Could not delete partner: ' . $partner_email . '. Could not find match.' . PHP_EOL;
-            exit();
+            $this->view->message = 'Could not delete partner: ' . $partner_email . '. Could not find match.' . PHP_EOL;
+            return;
         }
 
         try {
             $model->delete($partner_id);
-            echo 'Successfully deleted partner: ' . $partner_email . PHP_EOL;
+            $this->view->message = 'Successfully deleted partner: ' . $partner_email . PHP_EOL;
         } catch (RuntimeException $e) {
-            echo 'Error deleting partner: ' . $partner_email . '. ' . $e->getMessage() . PHP_EOL;
+            $this->view->message = 'Error deleting partner: ' . $partner_email . '. ' . $e->getMessage() . PHP_EOL;
         }
 
     }
