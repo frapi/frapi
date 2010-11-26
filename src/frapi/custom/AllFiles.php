6db63a70d83fa85c50f88e730d4c212de6e7f15b
David Coallier <david@echolibre.com>
2010-11-26 21:56:54 +0000

    - There's enough code and we can recommend people to use ArmChair, no need to polute the global scope anymore and it'll be easier for people if there are less dependencies.

--- src/frapi/custom/AllFiles.php
+++ src/frapi/custom/AllFiles.php
@@ -6,8 +6,4 @@ define('FRAPI_CACHE_ADAPTER', 'apc');
 // IE: require CUSTOM_MODEL . DIRECTORY_SEPARATOR . 'ModelName.php';
 // Or add an autolaoder if you are brave.
 
-// Frapi comes with Armchair by default. You can use it or decide to remove it.
-// You can find armchair at: git://github.com/till/armchair.git
-//require CUSTOM_MODEL . DIRECTORY_SEPARATOR . 'ArmChair' . DIRECTORY_SEPARATOR . 'ArmChair.php';
-
 // Other data
