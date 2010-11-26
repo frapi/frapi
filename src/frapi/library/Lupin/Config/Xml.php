c48c1ebbe22d3d984d49b0ea4cee33241587a00f
David Coallier <david@echolibre.com>
2010-11-26 22:56:27 +0000

    - Added a configIsWriteable() validation whenever we are about to save to the configuration file.

--- src/frapi/library/Lupin/Config/Xml.php
+++ src/frapi/library/Lupin/Config/Xml.php
@@ -13,8 +13,8 @@ class Lupin_Config_Xml_Exception extends Exception {}
  * obtain it through the world-wide-web, please send an email
  * to license@getfrapi.com so we can send you a copy immediately.
  *
- * This class deals with anything taht reads, saves, updates, delete 
- * information in the configuration files. Needless to say, this is a 
+ * This class deals with anything taht reads, saves, updates, delete
+ * information in the configuration files. Needless to say, this is a
  * critical file.
  *
  * @license   New BSD
@@ -30,14 +30,14 @@ class Lupin_Config_Xml
      * @var string $configFile  The location of the xml configuration.
      */
     protected $configFile;
-    
+
     /**
      * The array of the configuration
      *
      * @var array $config The configuration for a module.
      */
     protected $config;
-    
+
     /**
      * This node disapears after the parse, however we need
      * to retain it for future use. IE(Saving)
@@ -45,12 +45,12 @@ class Lupin_Config_Xml
      * @var string  The name of the config
      */
     protected $configName;
-    
+
     /**
      * Constructor
      *
      * This constructor fetches the local configuration path
-     * and constructs the config file for a certain module. 
+     * and constructs the config file for a certain module.
      *
      * After having the xml configuration file loaded, it'll create
      * a Zend_Config_Xml instance and store it in $this->config
@@ -66,15 +66,15 @@ class Lupin_Config_Xml
                 $config = Zend_Registry::get('localConfigPath');
             } catch (Zend_Exception $e) {}
         }
-        
+
         $this->configFile = $config . $name . '.xml';
         $this->configName = $name;
-        
+
         /**
-         * @TODO Remove me after the rewrite of the Config Reader. Move this to 
+         * @TODO Remove me after the rewrite of the Config Reader. Move this to
          *       Frapi_Internal. I dislike inter-dependencies.
          */
-        if ($cachedConfig = Frapi_Internal::getCached('configFile-' . $name)) 
+        if ($cachedConfig = Frapi_Internal::getCached('configFile-' . $name))
         {
             $this->config = $cachedConfig;
         } else {
@@ -85,7 +85,24 @@ class Lupin_Config_Xml
 
         $this->config = $this->config[$name];
     }
-    
+
+    /**
+     * Verify if the config file is still writeable.
+     *
+     * This method is used whenever we are about to write to
+     * the config file. We validate whether or not we can
+     * write to it and if we can't we simply throw an exception.
+     *
+     * @throws Exception
+     * @return void
+     */
+    private function configIsWriteable()
+    {
+        if (!is_writeable($this->configFile)) {
+            throw new Lupin_Config_Xml_Exception('File not writeable');
+        }
+    }
+
     /**
      * Get a single entry by field and value
      *
@@ -106,7 +123,7 @@ class Lupin_Config_Xml
 
             foreach ($this->config[$type] as $key => $node) {
                 if (isset($node[$field]) && $node[$field] == $value &&
-                    isset($this->config[$type][$key]) && isset($this->config[$type][$key][$field])) 
+                    isset($this->config[$type][$key]) && isset($this->config[$type][$key][$field]))
                 {
                     return $this->config[$type][$key];
                 }
@@ -115,7 +132,7 @@ class Lupin_Config_Xml
 
         return false;
     }
-    
+
     /**
      * Get all entries of a type
      *
@@ -130,21 +147,21 @@ class Lupin_Config_Xml
         if (!isset($this->config[$type])) {
             return false;
         }
-        
+
         $ret = $this->config[$type];
-        
+
         if (empty($ret)) {
             return false;
         }
-        
+
         return $this->normalize($ret);
     }
-    
+
     /**
      * Delete a node from the configuration
      *
      * This API may seem a bit confusing, however the three
-     * fields are used to identify the node we want to find, 
+     * fields are used to identify the node we want to find,
      * the value of the current node and what to compare it against.
      *
      * Example (infos.xml):
@@ -166,7 +183,7 @@ class Lupin_Config_Xml
      * ?>
      *
      * The result XML will be:
-     * 
+     *
      * <config>
      *   <infos>
      *     <name>
@@ -179,7 +196,7 @@ class Lupin_Config_Xml
      * @param  string $type   The node to retrieve.
      * @param  string $field  The field within that node.
      * @param  string $value  The expected value of that field to retrieve and delete
-     * 
+     *
      * @return bool           The result from the $this->save();
      */
     public function deleteByField($type, $field, $value)
@@ -188,17 +205,17 @@ class Lupin_Config_Xml
             if (!isset($this->config[$type][0])) {
                 $this->config[$type] = array($this->config[$type]);
             }
-            
+
             foreach ($this->config[$type] as $key => $node) {
                 if (isset($node[$field]) && $node[$field] == $value &&
-                    isset($this->config[$type][$key])) 
+                    isset($this->config[$type][$key]))
                 {
                     unset($this->config[$type][$key]);
                 }
-                
+
                 // This is a bug that happens when there's no first key. We need
                 // to make sure [0] exists in the array.
-                if (isset($this->config[$type]) && !empty($this->config[$type]) && 
+                if (isset($this->config[$type]) && !empty($this->config[$type]) &&
                     !isset($this->config[$type][0]))
                 {
                     $conf = array_merge(array(), $this->config[$type]);
@@ -209,7 +226,7 @@ class Lupin_Config_Xml
 
         return $this->save();
     }
-    
+
     /**
      * Edit an existing XML node
      *
@@ -219,11 +236,11 @@ class Lupin_Config_Xml
      *
      * @param  string $type   The node to retrieve.
      * @param  string $field  The field within that node.
-     * @param  string $value  The expected value of that field 
+     * @param  string $value  The expected value of that field
      *                        to retrieve and delete
      * @param  array  $data   The data to update the node with.
      *
-     * @return boolean Either false if the save was a failure 
+     * @return boolean Either false if the save was a failure
      *                 or true if it was a success
      */
     public function update($type, $field, $value, array $data)
@@ -235,7 +252,7 @@ class Lupin_Config_Xml
 
             foreach ($this->config[$type] as $key => $node) {
                 if (isset($node[$field]) && $node[$field] == $value &&
-                    isset($this->config[$type][$key])) 
+                    isset($this->config[$type][$key]))
                 {
                     $values = $this->config[$type][$key];
                     $conf = array_merge($values, $data);
@@ -245,15 +262,15 @@ class Lupin_Config_Xml
                 }
             }
         }
-        
+
         return false;
     }
-    
+
     /**
      * Add a new node of data to the configuration
      *
      * This method is used to append/add new data into the xml
-     * configuration files. 
+     * configuration files.
      *
      * By passing the array of fields, the data is going to be added
      * to the node of supplied "$type".
@@ -261,7 +278,7 @@ class Lupin_Config_Xml
      * @param string $type   The node to append to.
      * @param array  $data   The data of the new node to append to.
      *
-     * @return mixed Either false or true depending on whether it was a 
+     * @return mixed Either false or true depending on whether it was a
      *               success or a failure.
      */
     public function add($type, array $data)
@@ -280,23 +297,23 @@ class Lupin_Config_Xml
             }
             // Generate a ... pseudo random hash.
             $data['hash'] = sha1(microtime(true) . mt_rand(1, 10000000));
-            
+
             $len = count($this->config[$type]);
             $this->config[$type][$len] = $data;
 
             return $this->save();
         }
-        
+
         return false;
     }
-    
+
     /**
      * Save the new configuration
      *
      * This method saves the newly updated configuration. Please note
      * that at it's basic stage, the config files have a <config> root
      * node, however after saving using the Zend_Config_Writer_Xml, the
-     * root node becomes <zend-config>. 
+     * root node becomes <zend-config>.
      *
      * Don't worry about it, the parser won't see a difference.
      *
@@ -306,10 +323,12 @@ class Lupin_Config_Xml
      */
     protected function save()
     {
+        $this->configIsWriteable();
+
         $helper = new Lupin_Config_Helper_XmlArray();
 
         Frapi_Internal::deleteCached('configFile-' . $this->configName);
-        
+
         return $helper->write(
             array(
                 'config'       => $this->config,
@@ -318,13 +337,13 @@ class Lupin_Config_Xml
             )
         );
     }
-    
+
     /**
      * Normalize the output
      *
      * This method is used to normalize the output from the
      * Zend_Config object because it is not the same format
-     * when there's only 1 key or multiple keys. 
+     * when there's only 1 key or multiple keys.
      *
      * For the sake of simplicity, we just make sure it's normalized
      * here and so we can use the output as we please without loads
@@ -338,8 +357,8 @@ class Lupin_Config_Xml
         $array = $conf;
         if (isset($array[0])) {
             return $array;
-        } 
-            
+        }
+
         return array($array);
     }
 }
