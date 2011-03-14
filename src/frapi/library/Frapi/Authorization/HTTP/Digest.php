aa56cb3a8b1ae9903a695f88e089327d60e6d465
David Coallier <david@echolibre.com>
2011-03-14 11:28:56 +0000

    Added the username to be in PHP_AUTH_USER for the users using our Authentication. It is a hack but it is very useful instead of having to create an http_digest_parse method. The PHP_AUTH_PASS is not there.

--- src/frapi/library/Frapi/Authorization/HTTP/Digest.php
+++ src/frapi/library/Frapi/Authorization/HTTP/Digest.php
@@ -28,49 +28,49 @@
  */
 class Frapi_Authorization_HTTP_Digest extends Frapi_Authorization implements Frapi_Authorization_Interface
 {
-    /** 
+    /**
      * The secret key
      *
-     * @var string The secret key 
-     */ 
+     * @var string The secret key
+     */
     public $secretKey = 'secretKey--&@72';
-     
-    /** 
+
+    /**
      * The digest opaque value
      *
      * @var string The opaque value
-     */ 
+     */
     public $opaque = 'opaque';
-    
-    /** 
+
+    /**
      * The name of the authentication realm
      *
      * @var string The authentication realm.
-     */ 
+     */
     public $realm = 'FRAPI';
-    
-    /** 
+
+    /**
      * The base url of the application to authenticate against.
      *
      * @var string The base url for the authentication of the application.
-     */ 
+     */
     public $baseUrl = '/';
-    
-    /** 
+
+    /**
      * The life length of the nonce value.
      *
      * @var integer The nonce life length.
      */
     public $nonceLife = 300;
 
-    /** 
+    /**
      * This variable is used to define whether or not the passwords
      * should be A1 hashed.
      *
      * @var boolean True or False.
      */
     public $passwordsHashed = true;
-    
+
     /**
      * Constructor
      *
@@ -80,8 +80,8 @@ class Frapi_Authorization_HTTP_Digest extends Frapi_Authorization implements Fra
     {
         $this->realm = $_SERVER['SERVER_NAME'];
     }
-    
-    /** 
+
+    /**
      * Send the Authentication digest
      *
      * This method is used to send the authentication
@@ -98,7 +98,7 @@ class Frapi_Authorization_HTTP_Digest extends Frapi_Authorization implements Fra
             'domain="' . $this->baseUrl . '", ' .
             'qop=auth, '.
             'algorithm=MD5, ' .
-            'nonce="' . $this->getNonce() . '", ' . 
+            'nonce="' . $this->getNonce() . '", ' .
             'opaque="' . $this->getOpaque() . '"'
         );
 
@@ -106,7 +106,7 @@ class Frapi_Authorization_HTTP_Digest extends Frapi_Authorization implements Fra
         echo 'HTTP Digest Authentication required for "' . $this->realm . '"';
         exit(0);
     }
-    
+
     /**
      * Get the nonce
      *
@@ -119,14 +119,14 @@ class Frapi_Authorization_HTTP_Digest extends Frapi_Authorization implements Fra
     {
         $time = ceil(time() / $this->nonceLife) * $this->nonceLife;
         return hash(
-            'md5', 
-            date('Y-m-d H:i', $time) . ':' . 
-                $_SERVER['REMOTE_ADDR'] . ':' . 
+            'md5',
+            date('Y-m-d H:i', $time) . ':' .
+                $_SERVER['REMOTE_ADDR'] . ':' .
                 $this->secretKey
         );
     }
-    
-    /** 
+
+    /**
      * Get the opaque
      *
      * This method returns the opaque value hashed in
@@ -138,7 +138,7 @@ class Frapi_Authorization_HTTP_Digest extends Frapi_Authorization implements Fra
     {
         return hash('md5', $this->opaque);
     }
-    
+
     /**
      * Authorize the request
      *
@@ -156,7 +156,7 @@ class Frapi_Authorization_HTTP_Digest extends Frapi_Authorization implements Fra
      *               return access to $this->send() which will pop up the authentication
      *               challenge once again.
      */
-    public function authorize() 
+    public function authorize()
     {
         if (!isset($_SERVER['PHP_AUTH_DIGEST'])) {
             return $this->send();
@@ -164,15 +164,16 @@ class Frapi_Authorization_HTTP_Digest extends Frapi_Authorization implements Fra
 
         $authorization = $_SERVER['PHP_AUTH_DIGEST'];
 
-        if (preg_match('/username="([^"]+)"/', $authorization, $username) && 
-            preg_match('/nonce="([^"]+)"/', $authorization, $nonce) && 
+        if (preg_match('/username="([^"]+)"/', $authorization, $username) &&
+            preg_match('/nonce="([^"]+)"/', $authorization, $nonce) &&
             preg_match('/response="([^"]+)"/', $authorization, $response) &&
-            preg_match('/opaque="([^"]+)"/', $authorization, $opaque) && 
-            preg_match('/uri="([^"]+)"/', $authorization, $uri) ) 
+            preg_match('/opaque="([^"]+)"/', $authorization, $opaque) &&
+            preg_match('/uri="([^"]+)"/', $authorization, $uri) )
         {
             $username   = $username[1];
             $requestURI = $_SERVER['REQUEST_URI'];
-            
+            $_SERVER['PHP_AUTH_USER'] = $username;
+
             if (strpos($requestURI, '?') !== false) {
                 $requestURI = substr($requestURI, 0, strlen($uri[1]));
             }
@@ -183,8 +184,8 @@ class Frapi_Authorization_HTTP_Digest extends Frapi_Authorization implements Fra
                 return $this->send();
             }
 
-            if ($this->getOpaque() == $opaque[1] && $requestURI == $uri[1] && 
-                $this->getNonce() == $nonce[1]) 
+            if ($this->getOpaque() == $opaque[1] && $requestURI == $uri[1] &&
+                $this->getNonce() == $nonce[1])
             {
                 $passphrase = hash('md5', "$username:{$this->realm}:{$users['api_key']}");
 
@@ -195,10 +196,10 @@ class Frapi_Authorization_HTTP_Digest extends Frapi_Authorization implements Fra
                 }
 
                 $a2 = md5($_SERVER['REQUEST_METHOD'] . ':' . $requestURI);
-                
-                if (preg_match('/qop="?([^,\s"]+)/', $authorization, $qop) && 
-                    preg_match('/nc=([^,\s"]+)/', $authorization, $nc) && 
-                    preg_match('/cnonce="([^"]+)"/', $authorization, $cnonce)) 
+
+                if (preg_match('/qop="?([^,\s"]+)/', $authorization, $qop) &&
+                    preg_match('/nc=([^,\s"]+)/', $authorization, $nc) &&
+                    preg_match('/cnonce="([^"]+)"/', $authorization, $cnonce))
                 {
                     $expectedResponse =
                         md5($a1.':'.$nonce[1].':'.$nc[1].':'.$cnonce[1].':'.$qop[1].':'.$a2);
@@ -210,10 +211,10 @@ class Frapi_Authorization_HTTP_Digest extends Frapi_Authorization implements Fra
                     return $username;
                 }
             }
-            
+
             return $this->send();
         }
-        
+
         return $this->send();
     }
 }
