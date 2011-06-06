0f076d5e55015f2bd6d901abc390e8b3aa9de924
ss10sb <ss10sb@gmail.com>
2011-06-06 14:54:00 -0700

    added documentation to Frapi_FileHelper

--- src/frapi/library/Frapi/FileHelper.php
+++ src/frapi/library/Frapi/FileHelper.php
@@ -13,8 +13,21 @@
  */
 class Frapi_FileHelper
 {
+    /**
+     * base action directory
+     * NOT USED
+     * @var string
+     */
     protected static $dir;
 
+    /**
+     * creates a normalized name from the Action name passed in
+     * with the default join of '/', returns a name matching the file name
+     * with the join of '_', returns the class name
+     * @param string $name
+     * @param string $join
+     * @return string
+     */
     public static function normalizeName($name, $join = '/')
     {
         if (substr($name, 0, 1) == '/') {
@@ -27,11 +40,21 @@ class Frapi_FileHelper
         return $name;
     }
 
+    /**
+     * calls ::normalizeName with a join character of '_' to return the class name
+     * @param string $name
+     * @return string
+     */
     public static function generateClassName($name)
     {
         return self::normalizeName($name, '_');
     }
 
+    /**
+     * creates the base directory for Action
+     * NOT USED CURRENTLY
+     * @return string
+     */
     public static function getBaseDir()
     {
         if (!self::$dir) {
