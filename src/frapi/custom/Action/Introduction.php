67f9a59d725ac38ef97ce27f8bbc310619c964e8
Davey Shafik <dshafik@engineyard.com>
2012-02-18 20:06:35 +0000

    Add apidocs by default to /

---
+++ src/frapi/custom/Action/Introduction.php
@@ -0,0 +1,116 @@
+<?php
+
+/**
+ * Action Introduction 
+ * 
+ * Array
+ * 
+ * @link http://getfrapi.com
+ * @author Frapi <frapi@getfrapi.com>
+ * @link /
+ */
+class Action_Introduction extends Frapi_Action implements Frapi_Action_Interface
+{
+
+    /**
+     * Required parameters
+     * 
+     * @var An array of required parameters.
+     */
+    protected $requiredParams = array();
+
+    /**
+     * The data container to use in toArray()
+     * 
+     * @var A container of data to fill and return in toArray()
+     */
+    private $data = array();
+
+    /**
+     * To Array
+     * 
+     * This method returns the value found in the database 
+     * into an associative array.
+     * 
+     * @return array
+     */
+    public function toArray()
+    {
+        return $this->data;
+    }
+
+    /**
+     * Default Call Method
+     * 
+     * This method is called when no specific request handler has been found
+     * 
+     * @return array
+     */
+    public function executeAction()
+    {
+        return $this->toArray();
+    }
+
+    /**
+     * Get Request Handler
+     * 
+     * This method is called when a request is a GET
+     * 
+     * @return array
+     */
+    public function executeGet()
+    {
+        return $this->toArray();
+    }
+
+    /**
+     * Post Request Handler
+     * 
+     * This method is called when a request is a POST
+     * 
+     * @return array
+     */
+    public function executePost()
+    {
+        return $this->toArray();
+    }
+
+    /**
+     * Put Request Handler
+     * 
+     * This method is called when a request is a PUT
+     * 
+     * @return array
+     */
+    public function executePut()
+    {
+        return $this->toArray();
+    }
+
+    /**
+     * Delete Request Handler
+     * 
+     * This method is called when a request is a DELETE
+     * 
+     * @return array
+     */
+    public function executeDelete()
+    {
+        return $this->toArray();
+    }
+
+    /**
+     * Head Request Handler
+     * 
+     * This method is called when a request is a HEAD
+     * 
+     * @return array
+     */
+    public function executeHead()
+    {
+        return $this->toArray();
+    }
+
+
+}
+
