45c9f85f8da745648415b525fbc1c9cfe1ede5d8
Davey Shafik <me@daveyshafik.com>
2012-04-23 08:24:37 -0400

    Correctly install APC on all versions (fixes 5.2)

--- src/frapi/tests/travis-ci/install-apc.sh
+++ src/frapi/tests/travis-ci/install-apc.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
-
-wget http://pecl.php.net/get/APC
+wget "http://pecl.php.net/get/APC"
 tar -xzf APC
-sh -c "cd APC-* && phpize && ./configure && sudo make install"
+cd APC-*
+phpize
+./configure
+sudo make install
+INI_DIR=`php --ini | grep "Scan for additional" | sed -e "s|.*:\s*||"`
+INI_FILE="$INI_DIR/apc.ini"
 echo "extension=apc.so
 [apc]
 apc.enabled = 1
-apc.enable_cli = 1" >> `php --ini | grep "Loaded Configuration" | sed -e "s|.*:\s*||"`
+apc.enable_cli = 1" > $INI_FILE
