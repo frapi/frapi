897436ed1cdfd63d47a9d4a4a04bd863b3e994f4
Davey Shafik <me@daveyshafik.com>
2012-04-28 18:18:54 -0400

    Revert "Always return an array"
    This reverts commit d6f3e80e55ecfd3c921316a38b2580191ff13c17.

--- src/frapi/library/Frapi/Controller/Api.php
+++ src/frapi/library/Frapi/Controller/Api.php
@@ -244,7 +244,7 @@ class Frapi_Controller_Api extends Frapi_Controller_Main
                   ->setActionParams($this->getParams())
                   ->setActionFiles($this->getFiles())
                   ->setAcceptParams(
-                      isset($this->options) && is_array($this->options)
+                      isset($this->options) && is_array($this->options) 
                       ? $this->options : array()
                   );
 
@@ -344,7 +344,7 @@ class Frapi_Controller_Api extends Frapi_Controller_Main
         $types = $this->parseAcceptHeader();
 
         if(empty($types)) {
-            return array('mimeType' => false, 'outputFormat' => false);
+            return false;
         }
 
         if ($this->formatSetByExtension) {
@@ -397,7 +397,7 @@ class Frapi_Controller_Api extends Frapi_Controller_Main
             }
         }
 
-        return array('mimeType' => false, 'outputFormat' => false);
+        return array();
     }
 
     /**
