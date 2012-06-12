37fa97434e5dffe2a1b89ef78d1e5dc2b84ea508
Trevor Morse <trevor.morse@gmail.com>
2012-06-11 22:50:10 -0300

    Changed back requiredness of console options.

--- src/frapi/admin/application/modules/console/controllers/ErrorsController.php
+++ src/frapi/admin/application/modules/console/controllers/ErrorsController.php
@@ -61,10 +61,10 @@ class Console_ErrorsController extends Zend_Controller_Action
         $options = new Zend_Console_Getopt(
             array (
                 'name|n=s'        => $this->tr->_('NAME'),
-                'code|c=i'        => $this->tr->_('HTTP_CODE'),
+                'code|c-i'        => $this->tr->_('HTTP_CODE'),
                 'message|m=s'     => $this->tr->_('ERROR_MESSAGE'),
-                'description|d=s' => $this->tr->_('DESCRIPTION'),
-                'httpphrase|h=s'  => $this->tr->_('HTTP_REASON_PHRASE'),
+                'description|d-s' => $this->tr->_('DESCRIPTION'),
+                'httpphrase|h-s'  => $this->tr->_('HTTP_REASON_PHRASE'),
             )
         );
 
